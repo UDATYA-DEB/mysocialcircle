545032e0504e0c60510be71e10a475bb61c25577
Udatya Deb <74970338+UDATYA-DEB@users.noreply.github.com>
2023-07-02 17:31:39 +0530

    v1.3

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,16 @@ import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
 
 import "./app.css";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="myprofile" element={<Profile />} />
+    </Routes>
   );
 }
 
